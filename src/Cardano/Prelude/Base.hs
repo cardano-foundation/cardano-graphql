339d7384c263a234ff553fe731bed2d7091c9e10
Rupert Horlick <rupert.horlick@iohk.io>
2018-10-24 13:10:13 -0400

    Export id from Control.Category as identity

--- src/Cardano/Prelude/Base.hs
+++ src/Cardano/Prelude/Base.hs
@@ -1,9 +1,15 @@
 module Cardano.Prelude.Base
        ( module X
+       , identity
        ) where
 
 import           Protolude as X hiding (Hashable, hash, hashUsing, hashWithSalt,
-                     (.))
+                     identity, (.))
 
-import           Control.Category as X
+import           Control.Category (id)
+import           Control.Category as X hiding (id)
 import           Numeric.Natural as X
+
+-- | Rename `id` to `identity` to allow `id` as a variable name
+identity :: Category cat => cat a a
+identity = id
