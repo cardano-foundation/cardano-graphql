82d850b3025194f97023121ccf9fb6c3d03a508f
ksaric <kristijan.saric@iohk.io>
2018-11-05 14:39:53 +0100

    Added improvements for Text output and generic length.

--- src/Cardano/Prelude/Base.hs
+++ src/Cardano/Prelude/Base.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Cardano.Prelude.Base
        ( module X
        , identity
+       , putTextLn
+       , length
        ) where
 
+import qualified Protolude as Y
 import           Protolude as X hiding (Hashable, hash, hashUsing, hashWithSalt,
-                     identity, (.))
+                     identity, (.), length)
+
+import           Data.Foldable (Foldable)
+import qualified Data.Text as T
 
 import           Control.Category (id)
 import           Control.Category as X hiding (id)
@@ -13,3 +21,23 @@ import           Numeric.Natural as X
 -- | Rename `id` to `identity` to allow `id` as a variable name
 identity :: Category cat => cat a a
 identity = id
+
+-- | Explicit output with @Text@ since that is what we want most of the time.
+-- We don't want to look at the type errors or warnings arising.
+putTextLn :: Text -> IO ()
+putTextLn = putStrLn
+
+-- Length which includes @Text@ as well as @Foldable@.
+class HasLength a where
+    length' :: a -> Int
+
+instance HasLength Text where
+    length' = T.length
+
+instance Foldable t => HasLength (t a) where
+    length' = Y.length
+
+-- | We can pass several things here, as long as they have length.
+length :: HasLength a => a -> Int
+length = length'
+
