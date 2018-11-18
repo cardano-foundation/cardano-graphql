0fba47830bd7b251f51bd8ddda97ea84a214c659
Rhys Bartels-Waller <rhyslbw@gmail.com>
2018-11-18 23:45:39 +1100

    StandardJS formatting

--- src/GraphQLServer.js
+++ src/GraphQLServer.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import https from 'https';
-import http from 'http';
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import https from 'https'
+import http from 'http'
 
 export default class GraphQLServer {
-  certificateService = null;
-  tlsEnabled = null;
-  apolloServer = null;
-  httpServer = null;
+  certificateService = null
+  tlsEnabled = null
+  apolloServer = null
+  httpServer = null
 
   constructor(schema, options = { tlsEnabled: false, certificateService: null }) {
-    this.certificateService = options.certificateService;
-    this.tlsEnabled = options.tlsEnabled;
+    this.certificateService = options.certificateService
+    this.tlsEnabled = options.tlsEnabled
     this.apolloServer = new ApolloServer({
       schema,
       introspection: true
-    });
-    const app = express();
-    this.apolloServer.applyMiddleware({ app });
+    })
+    const app = express()
+    this.apolloServer.applyMiddleware({ app })
     if (this.tlsEnabled && this.certificateService) {
       this.httpServer = https.createServer({
         ca: this.certificateService.caStore,
@@ -25,27 +25,27 @@ export default class GraphQLServer {
         key: this.certificateService.privateKeyPem,
         requestCert: true,
         rejectUnauthorized: false
-      }, app);
+      }, app)
     } else {
-      this.httpServer = http.createServer(app);
+      this.httpServer = http.createServer(app)
     }
   }
 
   listen(...args) {
-    return this.httpServer.listen(...args);
+    return this.httpServer.listen(...args)
   }
 
   endpoint(port) {
-    const protocol = this.tlsEnabled ? 'https' : 'http';
-    return `${protocol}://localhost:${port}${this.graphqlPath}`;
+    const protocol = this.tlsEnabled ? 'https' : 'http'
+    return `${protocol}://localhost:${port}${this.graphqlPath}`
   }
 
   get graphqlPath() {
-    return this.apolloServer.graphqlPath;
+    return this.apolloServer.graphqlPath
   }
 
   shutdown(callback) {
-    this.httpServer.close(callback);
+    this.httpServer.close(callback)
   }
 
 }
--- test/integration.test.js
+++ test/integration.test.js
@@ -1,23 +1,23 @@
 import path from 'path'
-import { upAll, down } from 'docker-compose';
-import Docker from 'dockerode';
-import delay from 'delay';
-import gql from 'graphql-tag';
-import on from 'wait-on';
-import fetch from 'node-fetch';
-import { execute, makePromise } from 'apollo-link';
-import { createHttpLink } from 'apollo-link-http';
-import { expect } from 'chai';
+import { upAll, down } from 'docker-compose'
+import Docker from 'dockerode'
+import delay from 'delay'
+import gql from 'graphql-tag'
+import on from 'wait-on'
+import fetch from 'node-fetch'
+import { execute, makePromise } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
+import { expect } from 'chai'
 
-const docker = new Docker();
-const composeConfig = { cwd: path.join(__dirname), log: true };
-const uri = 'http://localhost:4000/graphql';
+const docker = new Docker()
+const composeConfig = { cwd: path.join(__dirname), log: true }
+const uri = 'http://localhost:4000/graphql'
 const healthChecks = [
   'http://localhost:4000/.well-known/apollo/server-health'
-];
-const link = createHttpLink({ uri, fetch });
+]
+const link = createHttpLink({ uri, fetch })
 
-const executeOperation = (operation) => makePromise(execute(link, operation));
+const executeOperation = (operation) => makePromise(execute(link, operation))
 
 async function allAddresses () {
   const response = await executeOperation({
@@ -27,8 +27,8 @@ async function allAddresses () {
                 id
             }
         }}`
-  });
-  return response.data.get_api_v1_addresses.data.map(a => a.id);
+  })
+  return response.data.get_api_v1_addresses.data.map(a => a.id)
 }
 
 async function accountAddresses (walletId) {
@@ -41,9 +41,9 @@ async function accountAddresses (walletId) {
     variables: {
       walletId
     }
-  });
+  })
   return response.data.get_api_v1_wallets_walletId_accounts.data
-    .reduce((addresses, account) => [...addresses, ...account.addresses.map(a => a.id)], []);
+    .reduce((addresses, account) => [...addresses, ...account.addresses.map(a => a.id)], [])
 }
 
 async function fetchDemoData () {
@@ -55,21 +55,21 @@ async function fetchDemoData () {
                 balance
             }
         }}`
-  });
-  const { data: { get_api_v1_wallets: { data: wallets }}} = walletResponse;
+  })
+  const { data: { get_api_v1_wallets: { data: wallets } } } = walletResponse
   if (wallets.length === 0) {
     // Demo cluster will eventually have demo wallet data, so keep retrying
-    await delay(1000);
-    return fetchDemoData();
+    await delay(1000)
+    return fetchDemoData()
   }
-  const sendingWallet = wallets[0];
-  const addresses = await allAddresses();
-  const sendingAccountAddresses = await accountAddresses(sendingWallet.id);
-  const recipientAddresses = addresses.filter(a => !sendingAccountAddresses.includes(a));
-  return { sendingWallet, recipientAddresses };
-};
+  const sendingWallet = wallets[0]
+  const addresses = await allAddresses()
+  const sendingAccountAddresses = await accountAddresses(sendingWallet.id)
+  const recipientAddresses = addresses.filter(a => !sendingAccountAddresses.includes(a))
+  return { sendingWallet, recipientAddresses }
+}
 
-async function getTransaction(id) {
+async function getTransaction (id) {
   const response = await executeOperation({
     query: gql`query GetTransaction($id: String!) {
         get_api_v1_transactions(id: $id) {
@@ -79,39 +79,38 @@ async function getTransaction(id) {
                 }
             }
         }}`,
-        variables: {
-          id
-        }
-  });
-  return response.data.get_api_v1_transactions.data;
+    variables: {
+      id
+    }
+  })
+  return response.data.get_api_v1_transactions.data
 }
 
-async function transactionPersisted(id) {
-  const transaction = await getTransaction(id);
-  if (transaction[0].status.tag === 'persisted' ) return true;
+async function transactionPersisted (id) {
+  const transaction = await getTransaction(id)
+  if (transaction[0].status.tag === 'persisted') return true
   // Keep checking until transaction is persisted in the blockchain
-  await delay(2000);
-  return transactionPersisted(id);
+  await delay(2000)
+  return transactionPersisted(id)
 }
 
 suite('Integration with the REST API as an adaptor', function () {
-
-  this.timeout(240000);
+  this.timeout(240000)
 
   suiteSetup(async function () {
-    await down(composeConfig);
-    await upAll(composeConfig);
-    await on({ resources: healthChecks });
-    this.demoData = await fetchDemoData();
-  });
+    await down(composeConfig)
+    await upAll(composeConfig)
+    await on({ resources: healthChecks })
+    this.demoData = await fetchDemoData()
+  })
 
-  suiteTeardown(async function() {
-    await down(composeConfig);
-    const volume = docker.getVolume('cardano-graphql_wallet-state');
-    await volume.remove({ force: true });
-  });
+  suiteTeardown(async function () {
+    await down(composeConfig)
+    const volume = docker.getVolume('cardano-graphql_wallet-state')
+    await volume.remove({ force: true })
+  })
 
-  test('Queries', async function() {
+  test('Queries', async function () {
     const result = await executeOperation({
       query: gql`query {
           get_api_v1_node_info {
@@ -123,14 +122,14 @@ suite('Integration with the REST API as an adaptor', function () {
               }
           }
       }`
-    });
-    expect(result.data).to.have.property('get_api_v1_node_info');
+    })
+    expect(result.data).to.have.property('get_api_v1_node_info')
     expect(result.data.get_api_v1_node_info.data.syncProgress)
-      .to.have.property('quantity').that.is.a('number');
-  });
+      .to.have.property('quantity').that.is.a('number')
+  })
 
-  test('Mutations', async function() {
-    const transactionAmount = 1250000;
+  test('Mutations', async function () {
+    const transactionAmount = 1250000
     const accountResponse = await executeOperation({
       query: gql`query GetWalletAccounts($walletId: String!) {
           get_api_v1_wallets_walletId_accounts(walletId: $walletId, page: 1, per_page: 50) {
@@ -142,8 +141,8 @@ suite('Integration with the REST API as an adaptor', function () {
       variables: {
         walletId: this.demoData.sendingWallet.id
       }
-    });
-    const { data: { get_api_v1_wallets_walletId_accounts: { data: accounts }}} = accountResponse;
+    })
+    const { data: { get_api_v1_wallets_walletId_accounts: { data: accounts } } } = accountResponse
 
     const estimatedFeesResponse = await executeOperation({
       query: gql`mutation FeeEstimation($index: Float!, $from: String!, $to: String!, $amount: Float!) {
@@ -166,8 +165,8 @@ suite('Integration with the REST API as an adaptor', function () {
         to: this.demoData.recipientAddresses[0],
         amount: transactionAmount
       }
-    });
-    const { estimatedAmount: fees } = estimatedFeesResponse.data.post_api_v1_transactions_fees.data;
+    })
+    const { estimatedAmount: fees } = estimatedFeesResponse.data.post_api_v1_transactions_fees.data
     const transferResult = await executeOperation({
       query: gql`mutation TransferMutation($index: Float!, $from: String!, $to: String!, $amount: Float!) {
           post_api_v1_transactions(body: {
@@ -189,13 +188,13 @@ suite('Integration with the REST API as an adaptor', function () {
         to: this.demoData.recipientAddresses[0],
         amount: transactionAmount
       }
-    });
-    const { data } = transferResult;
-    expect(data).to.have.property('post_api_v1_transactions');
+    })
+    const { data } = transferResult
+    expect(data).to.have.property('post_api_v1_transactions')
     expect(data.post_api_v1_transactions.data)
-      .to.have.property('id').that.is.a('string');
-    const { id } = data.post_api_v1_transactions.data;
-    await transactionPersisted(id);
+      .to.have.property('id').that.is.a('string')
+    const { id } = data.post_api_v1_transactions.data
+    await transactionPersisted(id)
     const walletResponse = await executeOperation({
       query: gql`query GetWalletById($walletId: String!){
           get_api_v1_wallets_walletId(walletId: $walletId) {
@@ -208,10 +207,9 @@ suite('Integration with the REST API as an adaptor', function () {
       variables: {
         walletId: this.demoData.sendingWallet.id
       }
-    });
-    const { data: { get_api_v1_wallets_walletId: { data: wallet }}} = walletResponse;
-    const newBalance = wallet.balance;
+    })
+    const { data: { get_api_v1_wallets_walletId: { data: wallet } } } = walletResponse
+    const newBalance = wallet.balance
     expect(newBalance).to.equal(this.demoData.sendingWallet.balance - transactionAmount - fees)
-  });
-
-});
+  })
+})
