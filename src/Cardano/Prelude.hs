f2502a2ab5d2a3733b4cc86bc9c62829ac2b48a0
Rupert Horlick <rupert.horlick@iohk.io>
2019-01-17 16:08:11 +0000

    Change MemSize to HeapWords

--- src/Cardano/Prelude.hs
+++ src/Cardano/Prelude.hs
@@ -7,7 +7,7 @@ import Cardano.Prelude.Base as X
 import Cardano.Prelude.Base16 as X
 import Cardano.Prelude.Error as X
 import Cardano.Prelude.Formatting as X
+import Cardano.Prelude.HeapWords as X
 import Cardano.Prelude.Json.Canonical as X
 import Cardano.Prelude.Json.Parse as X
-import Cardano.Prelude.MemSize as X
 import Cardano.Prelude.Orphans ()
