e1deb9ffd9cd376b185b005bff739bbd2fb1fd3e
Jordan Millar <jordan.millar@iohk.io>
2019-02-12 10:08:18 -0400

    [#30] Add `assertEitherIsRight` & `assertEitherIsLeft` functions

--- test/Test/Cardano/Prelude.hs
+++ test/Test/Cardano/Prelude.hs
@@ -6,6 +6,7 @@ where
 import Test.Cardano.Prelude.Base16 as X
 import Test.Cardano.Prelude.Gen as X
 import Test.Cardano.Prelude.Golden as X
+import Test.Cardano.Prelude.Helpers as X
 import Test.Cardano.Prelude.Orphans ()
 import Test.Cardano.Prelude.QuickCheck.Arbitrary as X
 import Test.Cardano.Prelude.QuickCheck.Property as X
