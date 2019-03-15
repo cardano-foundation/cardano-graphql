2256fd727c5f92e6218afdcf8cddf6e01c4a9dcd
Michael Hueschen <michaelhueschen@gmail.com>
2019-03-15 00:47:17 -0400

    Build internal haddocks in CI (#36)

--- scripts/buildkite/rebuild.hs
+++ scripts/buildkite/rebuild.hs
@@ -41,7 +41,13 @@ buildStep = do
  where
   cfg = ["--dump-logs", "--color", "always"]
   stackBuild args = run "stack" $ cfg ++ ["build", "--fast"] ++ args
-  buildArgs    = ["--bench", "--no-run-benchmarks", "--no-haddock-deps"]
+  buildArgs =
+    [ "--bench"
+    , "--no-run-benchmarks"
+    , "--haddock"
+    , "--haddock-internal"
+    , "--no-haddock-deps"
+    ]
   buildAndTest = stackBuild $ ["--tests"] ++ buildArgs
   build        = stackBuild $ ["--no-run-tests"] ++ buildArgs
   test         = stackBuild ["--test", "--jobs", "1"]
