30f61d7770dbb179c4c6cdcdf8ccd05286b00ef8
Duncan Coutts <duncan@well-typed.com>
2019-07-31 10:49:00 +0100

    Use hackage releases of libs, and downgrade to lts-13.26
    Use the new hackage releases of cborg and canonical-json.
    
    Downgrade from stackage lts-13.28 to lts-13.26. No dependent packages
    were actually using this because the nix setup isn't ready for it yet.
    
    Downgrading to match reality makes it easier for our tools
    (cardano-repo-tool) to generate the right info. The tool uses the
    stackage snapshot from this repo for the generated snapshot for all
    other repos. So it's really helpful if this one is a working one,
    otherwise the generated stuff has to be edited by hand.

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -6,16 +6,16 @@
         "hedgehog" = (((hackage.hedgehog)."1.0").revisions).default;
         "micro-recursion-schemes" = (((hackage.micro-recursion-schemes)."5.0.2.2").revisions).default;
         "streaming-binary" = (((hackage.streaming-binary)."0.3.0.1").revisions).default;
+        "cborg" = (((hackage.cborg)."0.2.2.0").revisions).default;
+        "canonical-json" = (((hackage.canonical-json)."0.6.0.0").revisions).default;
         } // {
         cardano-prelude = ./cardano-prelude.nix;
         cardano-prelude-test = ./cardano-prelude-test.nix;
-        cborg = ./cborg.nix;
         cardano-crypto = ./cardano-crypto.nix;
-        canonical-json = ./canonical-json.nix;
         };
       compiler.version = "8.6.5";
       compiler.nix-name = "ghc865";
       };
-  resolver = "lts-13.28";
+  resolver = "lts-13.26";
   compiler = "ghc-8.6.5";
   }
