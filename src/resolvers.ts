b17a44cea421b95b8fa3816efeab85ade49854f1
rhyslbw <rhyslbw@gmail.com>
2019-08-21 16:45:52 +1000

    Remove Entity resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,13 +1,6 @@
 import { Resolvers } from './graphql_types'
 
 export const resolvers: Resolvers = {
-  Entity: {
-    // obj should be typed correctly, but the compiler is complaining
-    // https://github.com/input-output-hk/cardano-graphql/issues/12
-    __resolveType: (obj: any) => {
-      if (obj.fee) return 'Transaction'
-    }
-  },
   Query: {
     block: (_root, args, { dataSources: { ledger } }) => ledger.blockById(args.id),
     transaction: (_root, args, { dataSources: { ledger } }) => ledger.transaction.load(args.id),
