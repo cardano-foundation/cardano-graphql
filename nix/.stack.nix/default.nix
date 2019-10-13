6c95f42116a9f01ffb2a37a032921736a9ab074c
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-14 08:36:17 +1100

    Nix voodoo

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -5,10 +5,10 @@
         "base58-bytestring" = (((hackage.base58-bytestring)."0.1.0").revisions).default;
         "hedgehog" = (((hackage.hedgehog)."1.0").revisions).default;
         "micro-recursion-schemes" = (((hackage.micro-recursion-schemes)."5.0.2.2").revisions).default;
+        "protolude" = (((hackage.protolude)."0.2.4").revisions).default;
         "streaming-binary" = (((hackage.streaming-binary)."0.3.0.1").revisions).default;
         "cborg" = (((hackage.cborg)."0.2.2.0").revisions).default;
         "canonical-json" = (((hackage.canonical-json)."0.6.0.0").revisions).default;
-        } // {
         cardano-prelude = ./cardano-prelude.nix;
         cardano-prelude-test = ./cardano-prelude-test.nix;
         cardano-crypto = ./cardano-crypto.nix;
@@ -17,5 +17,6 @@
       compiler.nix-name = "ghc865";
       };
   resolver = "lts-13.26";
+  modules = [ ({ lib, ... }: { packages = {}; }) { packages = {}; } ];
   compiler = "ghc-8.6.5";
   }
