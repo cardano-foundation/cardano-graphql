c90ff88811d857c2cbc100866e12eff304b477ed
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-16 08:41:15 +1100

    Nix voodoo

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -5,7 +5,6 @@
         "base58-bytestring" = (((hackage.base58-bytestring)."0.1.0").revisions).default;
         "hedgehog" = (((hackage.hedgehog)."1.0").revisions).default;
         "micro-recursion-schemes" = (((hackage.micro-recursion-schemes)."5.0.2.2").revisions).default;
-        "protolude" = (((hackage.protolude)."0.2.4").revisions).default;
         "streaming-binary" = (((hackage.streaming-binary)."0.3.0.1").revisions).default;
         "cborg" = (((hackage.cborg)."0.2.2.0").revisions).default;
         "canonical-json" = (((hackage.canonical-json)."0.6.0.0").revisions).default;
