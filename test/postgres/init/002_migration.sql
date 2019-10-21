0793c2563bc5571e9a6facb60b256f24d539ad30
Sam Jeston <Sam-Jeston@users.noreply.github.com>
2019-10-21 19:32:04 +1000

    Add timestamps, derived from block data (#47)

--- test/postgres/init/002_migration.sql
+++ test/postgres/init/002_migration.sql
@@ -1,4 +1,3 @@
--- TODO: Can be removed once new cexplorer received
 ALTER TABLE "block" ADD COLUMN "epoch_no" uinteger NULL;
 
 -- Hand written SQL to insert the epoch_no field.
@@ -13,3 +12,26 @@ UPDATE block SET epoch_no =
     END
   FROM block AS previous_block
   WHERE block.previous = previous_block.id AND block.epoch_no IS NULL ;
+
+ALTER TABLE "block" ADD COLUMN "time" timestamp NULL;
+
+-- Hand written SQL to populate the new column and then make it non-NULL.
+-- First timestamp the genesis block.
+update block set time = (select start_time from meta) where epoch_no is null;
+
+-- Then add timestamps to the EBBs.
+update block
+  set time =
+    (select cast (quote_literal (epoch_no * (select slot_duration from meta) * 21.6) as interval)
+        + (select start_time from meta))
+  where slot_no is null and epoch_no is not null;
+
+-- Then add timestamp to the main blocks.
+update block
+  set time =
+    (select cast (quote_literal (slot_no * (select slot_duration from meta) * 0.001) as interval)
+        + (select start_time from meta))
+  where slot_no is not null;
+
+-- Finally make the timestamp non-NULLable.
+alter table block alter column time set not null;
