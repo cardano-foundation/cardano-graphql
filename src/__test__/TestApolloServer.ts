35860b8e5805c7ff07800c67fe456eeb736991f6
Rhys Bartlels-Waller <rhys.bartelswaller@iohk.io>
2019-11-02 22:49:23 +1100

    Add note about why we're using the ApolloServerBase for integration testing

--- src/__test__/TestApolloServer.ts
+++ src/__test__/TestApolloServer.ts
@@ -7,6 +7,7 @@ import { getConfig } from '../config'
 export async function TestApolloServer (): Promise<ApolloServerBase> {
   process.env.HASURA_URI = 'http://localhost:8090/v1/graphql'
   const { context, resolvers } = await getConfig()
+  // ApolloServer has express encapsulated, so the 'apollo-server-testing' package requires the base
   return new ApolloServerBase({
     context,
     introspection: true,
