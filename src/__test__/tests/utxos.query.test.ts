d2e5a8083028dd5433bd5ab0c2deaf9fcdfdf715
Rhys Bartlels-Waller <rhys.bartelswaller@iohk.io>
2019-11-05 17:53:05 +1100

    Add all aggregate queries to top level, adding UTXO aggregate, and reworking utxoSet to be similar to others as utxo

--- src/__test__/tests/utxos.query.test.ts
+++ src/__test__/tests/utxos.query.test.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 import { TestClient } from '../TestClient'
 
-export function utxoSetTests (createClient: () => Promise<TestClient>) {
-  describe('utxoSet', () => {
+export function utxosTests (createClient: () => Promise<TestClient>) {
+  describe('utxos', () => {
     let client: TestClient
 
     beforeEach(async () => {
@@ -12,7 +12,7 @@ export function utxoSetTests (createClient: () => Promise<TestClient>) {
     it('Can be scoped by address', async () => {
       const result = await client.query({
         query: gql`query {
-            utxoSet(
+            utxos(
                 order_by: { address: asc }
                 where: { address: { _eq:
                 "DdzFFzCqrhskotfhVwhLvNFaVGpA6C4yR9DXe56oEL4Ewmze51f1uQsc1cQb8qUyqgzjUPBgFZiVbuQu7BaXrQkouyvzjYjLqfJpKG5s"
@@ -24,13 +24,13 @@ export function utxoSetTests (createClient: () => Promise<TestClient>) {
             }
         }`
       })
-      expect(result.data.utxoSet.length).toBe(2)
+      expect(result.data.utxos.length).toBe(2)
       expect(result.data).toMatchSnapshot()
     })
     it('Can be scoped by list of addresses', async () => {
       const result = await client.query({
         query: gql`query {
-            utxoSet(
+            utxos(
                 order_by: { address: asc }
                 where: { address: { _in: [
                     "DdzFFzCqrhskotfhVwhLvNFaVGpA6C4yR9DXe56oEL4Ewmze51f1uQsc1cQb8qUyqgzjUPBgFZiVbuQu7BaXrQkouyvzjYjLqfJpKG5s",
@@ -42,7 +42,32 @@ export function utxoSetTests (createClient: () => Promise<TestClient>) {
             }
         }`
       })
-      expect(result.data.utxoSet.length).toBe(3)
+      expect(result.data.utxos.length).toBe(3)
+      expect(result.data).toMatchSnapshot()
+    })
+    it('Can return aggregated UTXO data', async () => {
+      const result = await client.query({
+        query: gql`query {
+            utxos_aggregate( where: { value: { _lt: "200000" }}) {
+                aggregate {
+                    avg {
+                        value
+                    }
+                    count
+                    max {
+                        value
+                    }
+                    min {
+                        value
+                    }
+                    sum {
+                        value
+                    }
+                }
+            }
+        }`
+      })
+      expect(result.data.utxos_aggregate.aggregate.count).toBe(68)
       expect(result.data).toMatchSnapshot()
     })
   })
