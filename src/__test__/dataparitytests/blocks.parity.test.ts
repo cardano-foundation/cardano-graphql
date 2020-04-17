6bd4f249abbfb8dfaf29fa8c0bb72795bee355b9
Edward Haigh <edward@haigh.io>
2020-04-17 11:43:09 +0100

    Refactored lets -> consts and ammended url argument

--- src/__test__/dataparitytests/blocks.parity.test.ts
+++ src/__test__/dataparitytests/blocks.parity.test.ts
@@ -2,13 +2,12 @@ import gql from 'graphql-tag'
 import { TestClient } from '../TestClient'
 import fetch from 'node-fetch'
 
-async function getDataFromAPI(url: string ) {
-    let response = await fetch(url)
-    let data = await response.json()
-    return data
+async function getDataFromAPI(path: string ) {
+    const response = await fetch("https://explorer.cardano.org/api/" + path)
+    return await response.json()
 }
 
-export function transactionTests(createClient: () => Promise<TestClient>) {
+export function blocksTests(createClient: () => Promise<TestClient>) {
 
     describe('blocks', () => {
         let client: TestClient
@@ -18,7 +17,7 @@ export function transactionTests(createClient: () => Promise<TestClient>) {
         }, 60000)
 
         it('returns the same height', async () => {
-            const restResult = await getDataFromAPI("https://explorer.cardano.org/api/blocks/pages")
+            const restResult = await getDataFromAPI("blocks/pages")
             const graphQLResult = await client.query({
                 query: gql`query Blockheight {
                     cardano {
@@ -27,8 +26,8 @@ export function transactionTests(createClient: () => Promise<TestClient>) {
                 }`
             })
 
-            let restResultBlockHeight = restResult["Right"][1][0]["cbeBlkHeight"]
-            let graphQLBlockHeight = graphQLResult["data"]["cardano"]["blockHeight"]
+            const restResultBlockHeight = restResult["Right"][1][0]["cbeBlkHeight"]
+            const graphQLBlockHeight = graphQLResult["data"]["cardano"]["blockHeight"]
 
             // As we're calling an external API to check equality on something that changes every 20 seconds
             // there is a small delta in the test condition to allow for this where the second API value can be
