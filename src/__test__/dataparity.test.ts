e917884a4f9e337e37b3415685b639eeb52e02ac
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-04-18 00:35:38 +1000

    Lint fix

--- src/__test__/dataparity.test.ts
+++ src/__test__/dataparity.test.ts
@@ -2,11 +2,11 @@ import * as tests from './dataparitytests'
 
 const { TEST_MODE } = process.env
 if (!TEST_MODE || (TEST_MODE !== 'e2e' && TEST_MODE !== 'integration')) {
-    throw new Error('Test mode must be provided (e2e || integration)')
+  throw new Error('Test mode must be provided (e2e || integration)')
 }
 
 const createClient = TEST_MODE === 'e2e'
-    ? require('./e2e_client').createClient
-    : require('./integration_client').createClient
+  ? require('./e2e_client').createClient
+  : require('./integration_client').createClient
 
 Object.values(tests).forEach(test => test(createClient))
--- src/__test__/dataparitytests/blocks.parity.test.ts
+++ src/__test__/dataparitytests/blocks.parity.test.ts
@@ -2,39 +2,38 @@ import gql from 'graphql-tag'
 import { TestClient } from '../TestClient'
 import fetch from 'node-fetch'
 
-async function getDataFromAPI(path: string ) {
-    const response = await fetch("https://explorer.cardano.org/api/" + path)
-    return await response.json()
+async function getDataFromAPI (path: string) {
+  const response = await fetch(`https://explorer.cardano.org/api/${path}`)
+  return response.json()
 }
 
-export function blocksTests(createClient: () => Promise<TestClient>) {
+export function blocksTests (createClient: () => Promise<TestClient>) {
+  describe('blocks', () => {
+    let client: TestClient
 
-    describe('blocks', () => {
-        let client: TestClient
+    beforeEach(async () => {
+      client = await createClient()
+    }, 60000)
 
-        beforeEach(async () => {
-            client = await createClient()
-        }, 60000)
-
-        it('returns the same height', async () => {
-            const restResult = await getDataFromAPI("blocks/pages")
-            const graphQLResult = await client.query({
-                query: gql`query Blockheight {
+    it('returns the same height', async () => {
+      const restResult = await getDataFromAPI('blocks/pages')
+      const graphQLResult = await client.query({
+        query: gql`query Blockheight {
                     cardano {
                         blockHeight
                     }    
                 }`
-            })
+      })
 
-            const restResultBlockHeight = restResult["Right"][1][0]["cbeBlkHeight"]
-            const graphQLBlockHeight = graphQLResult["data"]["cardano"]["blockHeight"]
+      const restResultBlockHeight = restResult['Right'][1][0]['cbeBlkHeight']
+      const graphQLBlockHeight = graphQLResult['data']['cardano']['blockHeight']
 
-            // As we're calling an external API to check equality on something that changes every 20 seconds
-            // there is a small delta in the test condition to allow for this where the second API value can be
-            // equal to or one more than the first API value. 
+      // As we're calling an external API to check equality on something that changes every 20 seconds
+      // there is a small delta in the test condition to allow for this where the second API value can be
+      // equal to or one more than the first API value.
 
-            expect(graphQLBlockHeight).toBeGreaterThan(restResultBlockHeight - 1)
-            expect(graphQLBlockHeight).toBeLessThanOrEqual(restResultBlockHeight + 1);
-        })
+      expect(graphQLBlockHeight).toBeGreaterThan(restResultBlockHeight - 1)
+      expect(graphQLBlockHeight).toBeLessThanOrEqual(restResultBlockHeight + 1)
     })
+  })
 }
