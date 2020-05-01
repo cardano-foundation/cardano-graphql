e653a90f9d1927794321855005d15dce93bd5cfe
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-05-01 14:46:28 +1000

    Align query interface between the Hasura derived schema and the Cardano GraphQL schema

--- src/buildHasuraSchema.ts
+++ src/buildHasuraSchema.ts
@@ -33,7 +33,7 @@ export async function buildHasuraSchema (hasuraUri: string) {
   // when provisioning a new deployment we need to protect against.
   await RetryPromise.retryPromise('Checking Hasura metadata is applied', async () => {
     const result = await makePromise(execute(link, { query: gql`
-            query { Block(where: {number: {_eq: 1}}) {
+            query { blocks(where: {number: {_eq: 1}}) {
                 transactions_aggregate { aggregate { count }}
             }}`
     }))
