185c38f4dbeaa1ab475fc87474fcce98b100b00b
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-05-01 19:00:59 +1000

    Refactor TDD test server config

--- src/__test__/TestApolloServer.ts
+++ src/__test__/TestApolloServer.ts
@@ -2,16 +2,24 @@ import { ApolloServerBase } from 'apollo-server-core'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as depthLimit from 'graphql-depth-limit'
-import { getConfig } from '../config'
+import { hasuraResolvers, scalarResolvers } from '../resolvers'
+import { Resolvers } from '../graphql_types'
+import { buildContext } from '../Context'
+
+/**
+ * A server compatible with apollo-server-testing
+ *
+ * Enables direct querying of the service, sans HTTP server.
+ *
+ * @packageDocumentation
+ */
 
 export async function TestApolloServer (): Promise<ApolloServerBase> {
-  process.env.HASURA_URI = 'http://localhost:8090/v1/graphql'
-  const { context, resolvers } = await getConfig()
-  // ApolloServer has express encapsulated, so the 'apollo-server-testing' package requires the base
+  const context = await buildContext('http://localhost:8090/v1/graphql')
   return new ApolloServerBase({
     context,
     introspection: true,
-    resolvers,
+    resolvers: Object.assign({}, scalarResolvers, hasuraResolvers) as Resolvers,
     typeDefs: fs.readFileSync(path.join(__dirname, '../schema.graphql'), 'UTF8'),
     validationRules: [depthLimit(20)]
   })
