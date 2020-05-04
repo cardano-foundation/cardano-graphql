971fa2735706a9d01726069158b1098b60994d5d
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-05-04 22:42:11 +1000

    Adds a top-level utility implementing hasura-allow-operations-in, providing the internal query requirements to be allowed

---
+++ src/util/hasuraAllowOperationsIn.ts
@@ -0,0 +1,23 @@
+import { run, RunReport } from 'hasura-allow-operations-in'
+import * as path from 'path'
+
+const appSource = process.argv[2]
+
+if (appSource === undefined) {
+  throw new Error('Source path must be passed as first argument')
+}
+
+if (process.env.HASURA_URI === undefined) {
+  throw new Error('HASURA_URI must be set')
+}
+
+const cardanoGraphQLOperations = path.resolve(__dirname, '..', '..', 'src', 'graphql_operations')
+
+const message = (result: RunReport, body: string) => {
+  const operationText = result.operationsFound > 1 ? 'operations were' : 'operation was'
+  console.log(`${result.operationsFound} ${operationText} ${body}`)
+}
+
+run(process.env.HASURA_URI, [`${cardanoGraphQLOperations}/**/*.graphql`, appSource], true)
+  .then(result => message(result, 'found and included in the allow list'))
+  .catch(error => console.error(error))
