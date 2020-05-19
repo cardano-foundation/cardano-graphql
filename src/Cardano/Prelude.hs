b6de17dfeb45674cf138fbb6f83f81081de9f651
Nicolas Frisby <nick.frisby@iohk.io>
2020-05-19 09:44:13 -0700

    Revert "Define gshowsPrecWithoutRecordSyntax"
    This reverts commit 6633e0f3572a29c37fa05a1b38b35ae1ae018bd3.

--- src/Cardano/Prelude.hs
+++ src/Cardano/Prelude.hs
@@ -12,5 +12,4 @@ import Cardano.Prelude.HeapWords as X
 import Cardano.Prelude.Json.Canonical as X
 import Cardano.Prelude.Json.Parse as X
 import Cardano.Prelude.Orphans ()
-import Cardano.Prelude.Show as X
 import Cardano.Prelude.Strict as X
