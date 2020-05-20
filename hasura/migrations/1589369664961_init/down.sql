861e657afbbd6ea6652a3e59cdf77da86156cc5b
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-05-20 14:43:19 +1000

    Fix invalid down migration

--- hasura/migrations/1589369664961_init/down.sql
+++ hasura/migrations/1589369664961_init/down.sql
@@ -1,2 +1,2 @@
-DROP VIEW "Block", "Cardano", "Epoch", "Transaction", "Utxo" "TransactionInput" "TransactionOutput"
-DROP FUNCTION utxo_set_at_block
+DROP VIEW IF EXISTS "Block", "Cardano", "Epoch", "Transaction", "Utxo", "TransactionInput", "TransactionOutput" CASCADE;
+DROP FUNCTION IF EXISTS utxo_set_at_block CASCADE;
