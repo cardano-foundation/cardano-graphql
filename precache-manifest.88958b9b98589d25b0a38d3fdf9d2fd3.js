d823b1ee25acd0688656998802597b32b09de8d3
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-06-17 14:19:34 +1000

    Updates

---
+++ precache-manifest.88958b9b98589d25b0a38d3fdf9d2fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fa32c593ced54a0cce1c77ddfe965a",
+    "url": "/cardano-graphql/index.html"
+  },
+  {
+    "revision": "b3f5efe22c21278035c0",
+    "url": "/cardano-graphql/static/js/2.fffeeaea.chunk.js"
+  },
+  {
+    "revision": "3278925c0ef75a80e43cac60a05cf3de",
+    "url": "/cardano-graphql/static/js/2.fffeeaea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04832560a5168dc26a3",
+    "url": "/cardano-graphql/static/js/main.55f5faf1.chunk.js"
+  },
+  {
+    "revision": "de9598eb8a9fb460d09d",
+    "url": "/cardano-graphql/static/js/runtime-main.cf8d782f.js"
+  }
+]);
