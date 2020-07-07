fa9571fe0bd13ebb03a4f44031b1076f5082173a
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-07-07 15:31:57 +1000

    Sleep debug

--- packages/api-cardano-db-hasura/hasura/docker-entrypoint.sh
+++ packages/api-cardano-db-hasura/hasura/docker-entrypoint.sh
@@ -8,7 +8,8 @@ POSTGRES_USER=${POSTGRES_USER:-$(cat ${SECRET_DIR}/postgres_user)}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-$(cat ${SECRET_DIR}/postgres_password)}
 HASURA_GRAPHQL_DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}
 
-
+echo "Sleeping to ensure migrations are applied after db-sync starts"
+sleep 15
 /bin/pre-start.sh \
   --db-url ${HASURA_GRAPHQL_DATABASE_URL} \
   --hasura-cli-path /bin/hasura-cli \
