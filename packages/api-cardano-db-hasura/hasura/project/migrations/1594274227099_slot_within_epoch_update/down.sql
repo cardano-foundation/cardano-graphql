00dc5349d5722248c2e5251b6b16c1910f5ddf50
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-07-09 16:36:12 +1000

    fix(hasura): replaces protocol constant calculation with new meta field (#221)
    Closes #194

---
+++ packages/api-cardano-db-hasura/hasura/project/migrations/1594274227099_slot_within_epoch_update/down.sql
@@ -0,0 +1,21 @@
+DROP VIEW if EXISTS "Block";
+CREATE VIEW "public"."Block" AS
+ SELECT (COALESCE(( SELECT sum((tx.fee)::bigint) AS sum
+           FROM tx
+          WHERE (tx.block = block.id)), (0)::numeric))::bigint AS fees,
+    block.hash,
+    block.merkel_root AS "merkelRoot",
+    block.block_no AS number,
+    previous_block.hash AS "previousBlockHash",
+    next_block.hash AS "nextBlockHash",
+    slot_leader.description AS "createdBy",
+    block.size,
+    block.tx_count AS "transactionsCount",
+    block.epoch_no AS "epochNo",
+    block.slot_no AS "slotNo",
+    block.slot_no - (block.epoch_no * (10 * (select protocol_const from meta))) as "slotWithinEpoch",
+    block."time" AS "createdAt"
+   FROM (((block
+     LEFT JOIN block previous_block ON ((block.previous = previous_block.id)))
+     LEFT JOIN block next_block ON ((next_block.previous = block.id)))
+     LEFT JOIN slot_leader ON ((block.slot_leader = slot_leader.id)));
