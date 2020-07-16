3f69349f57704acf1445c958e4f73df90820a246
John Ky <newhoggy@gmail.com>
2020-07-17 08:26:25 +1000

    Update index-state to 2020-07-15. Update nix build

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -13,33 +13,44 @@
 }:
 let
 
+  src = haskell-nix.haskellLib.cleanGit {
+      name = "cardano-prelude-src";
+      src = ../.;
+  };
+
   # This creates the Haskell package set.
   # https://input-output-hk.github.io/haskell.nix/user-guide/projects/
-  pkgSet = haskell-nix.cabalProject {
-      src = haskell-nix.haskellLib.cleanGit { src = ../.; };
-      ghc = buildPackages.haskell-nix.compiler.${compiler};
+  pkgSet = haskell-nix.cabalProject  (lib.optionalAttrs stdenv.hostPlatform.isWindows {
+    # FIXME: without this deprecated attribute, db-converter fails to compile directory with:
+    # Encountered missing dependencies: unix >=2.5.1 && <2.9
+    ghc = buildPackages.haskell-nix.compiler.${compiler};
+  } // {
+    inherit src;
+    compiler-nix-name = compiler;
+    #ghc = buildPackages.haskell-nix.compiler.${compiler};
     modules = [
-        # Allow reinstallation of Win32
-        { nonReinstallablePkgs =
-          [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
-            "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
-            # ghcjs custom packages
-            "ghcjs-prim" "ghcjs-th"
-            "ghc-boot"
-            "ghc" "array" "binary" "bytestring" "containers"
-            "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
-            # "ghci" "haskeline"
-            "hpc"
-            "mtl" "parsec" "text" "transformers"
-            "xhtml"
-            # "stm" "terminfo"
-          ];
-        }
-        {
+      { compiler.nix-name = compiler; }
+      # Allow reinstallation of Win32
+      { nonReinstallablePkgs =
+        [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+          "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+          # ghcjs custom packages
+          "ghcjs-prim" "ghcjs-th"
+          "ghc-boot"
+          "ghc" "array" "binary" "bytestring" "containers"
+          "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+          # "ghci" "haskeline"
+          "hpc"
+          "mtl" "parsec" "text" "transformers"
+          "xhtml"
+          # "stm" "terminfo"
+        ];
+      }
+      {
           packages.cardano-prelude.configureFlags = [ "--ghc-option=-Werror" ];
           enableLibraryProfiling = profiling;
-        }
+      }
     ];
-  };
+  });
 in
   pkgSet
--- scripts/buildkite/default.nix
+++ scripts/buildkite/default.nix
@@ -8,7 +8,7 @@ with pkgs.lib;
 with pkgs;
 
 let
-  inherit (iohkNix) cache-s3;
+  cache-s3 = callPackage ./cache-s3.nix {};
 
   stackRebuild = runCommand "stack-rebuild" {} ''
     ${haskellPackages.ghcWithPackages (ps: [ps.turtle ps.safe ps.transformers])}/bin/ghc -o $out ${./rebuild.hs}
