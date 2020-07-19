0bdb34c07be3b02b577af25466580580acce1e5e
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-07-19 21:13:09 +1000

    feat(client-ts): include api-genesis in client-ts

---
+++ packages/client-ts/scripts/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+CLIENT_TS_ROOT="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+shx rm -rf \
+  $CLIENT_TS_ROOT/api/**/*/*index.d.ts \
+  $CLIENT_TS_ROOT/api/**/*/*graphql_types* \
+  $CLIENT_TS_ROOT/api/**/*/*.graphql
