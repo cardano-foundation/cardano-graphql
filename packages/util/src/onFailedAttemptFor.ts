e24df95dabad423f925ca061ffd668a7f1d05be2
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-08-05 00:56:58 +1000

    feat: Improve logging during retry attempts

--- packages/util/src/onFailedAttemptFor.ts
+++ packages/util/src/onFailedAttemptFor.ts
@@ -2,6 +2,7 @@ import { FailedAttemptError } from 'p-retry'
 
 export const onFailedAttemptFor = (operation: string) => ({ attemptNumber, message, retriesLeft }: FailedAttemptError) => {
   const nextAction = retriesLeft > 0 ? 'retying...' : 'exiting'
+  console.warn(message)
   console.log(`${operation}: Attempt ${attemptNumber} of ${attemptNumber + retriesLeft}, ${nextAction}`)
   if (retriesLeft === 0) {
     console.error(message)
