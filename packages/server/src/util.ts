298c86cdcdab97d976d18626f55c29c0e487b674
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-09-09 15:41:34 +1000

    feature: make server listen address configurable

--- packages/server/src/util.ts
+++ packages/server/src/util.ts
@@ -1,9 +1,9 @@
 import { Application } from 'express'
 import http from 'http'
 
-export function listenPromise (app: Application, config: { port: number }): Promise<http.Server> {
+export function listenPromise (app: Application, port: number, hostname?: string): Promise<http.Server> {
   return new Promise(function (resolve, reject) {
-    const server: http.Server = app.listen(config.port, () => resolve(server))
+    const server: http.Server = app.listen(port, hostname, () => resolve(server))
     server.on('error', reject)
   })
 }
