d1d54a39cba501159144fc1f97face607a2a218f
John Ky <john.ky@iohk.io>
2020-10-15 09:36:43 +1100

    Copy code from base16-bytestring, Data.ByteString.Base16 to facilitate migration from base16-bytestring-0.1.1.7 to base16-bytestring-1.0.0.0
    This will unblock upgrade of base16-bytestring to base16-bytestring-1.0.0.0 in a subsequence commit.
    
    New function decodeEitherBase16 that behaves just like decode from base16-bytestring-1.0.0.0

--- cardano-prelude-test/test/test.hs
+++ cardano-prelude-test/test/test.hs
@@ -1,18 +1,19 @@
 module Main
   ( main
-  )
-where
+  ) where
 
 import Cardano.Prelude
 import Test.Cardano.Prelude
 
+import qualified Test.Cardano.Prelude.Base16Spec
 import qualified Test.Cardano.Prelude.GHC.Heap.NormalFormSpec
 import qualified Test.Cardano.Prelude.GHC.Heap.SizeSpec
 import qualified Test.Cardano.Prelude.GHC.Heap.TreeSpec
 
 main :: IO ()
 main = runTests
-  [ Test.Cardano.Prelude.GHC.Heap.NormalFormSpec.tests
+  [ Test.Cardano.Prelude.Base16Spec.tests
+  , Test.Cardano.Prelude.GHC.Heap.NormalFormSpec.tests
   , Test.Cardano.Prelude.GHC.Heap.SizeSpec.tests
   , Test.Cardano.Prelude.GHC.Heap.TreeSpec.tests
   ]
--- cardano-prelude/src/Cardano/Prelude/Base16.hs
+++ cardano-prelude/src/Cardano/Prelude/Base16.hs
@@ -3,19 +3,23 @@
 -- | Helper functions for parsing
 
 module Cardano.Prelude.Base16
-  ( parseBase16
-  , Base16ParseError(..)
+  ( Base16ParseError(..)
+
+  , decodeEitherBase16
+  , parseBase16
   )
 where
 
 import Cardano.Prelude.Base
-
-import qualified Data.ByteString.Base16 as B16
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.Text.Encoding as Text
+import Data.String
 import Formatting (bprint, shown)
 import Formatting.Buildable (Buildable(build))
 
+import qualified Cardano.Prelude.Base16.Internal as B16
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text.Encoding as Text
+
+{- HLINT ignore "Use isDigit" -}
 
 newtype Base16ParseError =
   Base16IncorrectSuffix ByteString
@@ -30,3 +34,13 @@ parseBase16 s = do
   let (bs, suffix) = B16.decode $ Text.encodeUtf8 s
   unless (BS.null suffix) . Left $ Base16IncorrectSuffix suffix
   pure bs
+
+decodeEitherBase16 :: ByteString -> Either String ByteString
+decodeEitherBase16 bs = case B16.decode bs of
+  (decodedBs, "") -> Right decodedBs
+  (_, _) -> Left $ "invalid character at offset: " <> show (BS.length (BS.takeWhile isHex bs))
+  where isHex :: Char -> Bool
+        isHex w =
+          (w >= '0' && w <= '9') ||
+          (w >= 'a' && w <= 'f') ||
+          (w >= 'A' && w <= 'F')
