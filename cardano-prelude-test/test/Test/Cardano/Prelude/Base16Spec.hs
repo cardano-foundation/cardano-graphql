be031d98df4e1d1b53e071ad3fa47de5fa3cbd16
John Ky <john.ky@iohk.io>
2020-10-17 15:58:56 +1100

    Make cardno prelude compile for multiple base16-bytestring versions

--- cardano-prelude-test/test/Test/Cardano/Prelude/Base16Spec.hs
+++ cardano-prelude-test/test/Test/Cardano/Prelude/Base16Spec.hs
@@ -16,7 +16,6 @@ import qualified Hedgehog.Range as R
 import qualified Data.ByteString.Base16 as B16
 import qualified Cardano.Prelude.Base16.Internal as I
 import qualified Data.ByteString as BS
-import qualified Data.Text.Encoding as T
 
 {- HLINT ignore "Reduce duplication" -}
 
@@ -28,19 +27,6 @@ prop_roundtrip = H.withTests 100 . H.property $ do
   bs <- H.forAll $ genByteString (R.linear 0 10) (G.word8 R.constantBounded)
   b16 <- H.forAll . pure $ B16.encode bs
   I.decode b16 === (bs, "")
-
-prop_noRegressionsValid :: Property
-prop_noRegressionsValid = H.withTests 100 . H.property $ do
-  bs <- H.forAll $ genByteString (R.linear 0 10) (G.word8 R.constantBounded)
-  b16 <- H.forAll . pure $ B16.encode bs
-  I.decode b16 === B16.decode b16
-
-prop_noRegressionsInvalidSuffix :: Property
-prop_noRegressionsInvalidSuffix = H.withTests 100 . H.property $ do
-  bs <- H.forAll $ genByteString (R.linear 0 10) (G.word8 R.constantBounded)
-  suffix <- H.forAll $ genByteString (R.linear 0 10) (G.element (BS.unpack (T.encodeUtf8 "qu")))
-  b16 <- H.forAll . pure $ B16.encode bs <> suffix
-  I.decode b16 === B16.decode b16
 
 prop_decodeEitherBase16_examples :: Property
 prop_decodeEitherBase16_examples = H.withTests 1 . H.property $ do
