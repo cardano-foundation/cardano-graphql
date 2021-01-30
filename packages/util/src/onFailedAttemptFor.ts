fbd695a3d1688b7be235ad21c6e0f9252564dd8f
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-01-30 23:12:32 +1100

    chore: minor logging improvements

--- packages/util/src/onFailedAttemptFor.ts
+++ packages/util/src/onFailedAttemptFor.ts
@@ -3,8 +3,8 @@ import { dummyLogger, Logger } from 'ts-log'
 
 export const onFailedAttemptFor = (operation: string, logger: Logger = dummyLogger) => ({ attemptNumber, message, retriesLeft }: FailedAttemptError) => {
   const nextAction = retriesLeft > 0 ? 'retrying...' : 'exiting'
-  logger.warn(message)
-  logger.info(`${operation}: Attempt ${attemptNumber} of ${attemptNumber + retriesLeft}, ${nextAction}`)
+  logger.trace(message)
+  logger.debug(`${operation}: Attempt ${attemptNumber} of ${attemptNumber + retriesLeft}, ${nextAction}`)
   if (retriesLeft === 0) {
     logger.error(message)
     process.exit(1)
