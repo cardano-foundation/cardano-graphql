1f02a245f08e42f8a5e19efe9051996cbb3916b9
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-03-28 15:23:20 +1100

    fix: Add order_by to asset query

--- packages/api-cardano-db-hasura/src/HasuraClient.ts
+++ packages/api-cardano-db-hasura/src/HasuraClient.ts
@@ -497,6 +497,7 @@ export class HasuraClient {
           assets (
               limit: $limit
               offset: $offset
+              order_by: { assetId: asc }
               where: {
                   metadataFetchAttempts: $metadataFetchAttempts
               }
