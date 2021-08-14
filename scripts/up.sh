a2247649c2cff19e4e51094a52b8edf7c5f830ab
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-08-14 13:17:22 +1000

    refactor: improve up script named argument handling

--- scripts/up.sh
+++ scripts/up.sh
@@ -7,11 +7,17 @@ export API_PORT=${API_PORT:-3100}
 export OGMIOS_PORT=${OGMIOS_PORT:-1337}
 export POSTGRES_PORT=${POSTGRES_PORT:-5433}
 
-if [[ $1 ]]; then
-  # Docker Buildkit
-  export DOCKER_BUILDKIT=1
-  export COMPOSE_DOCKER_CLI_BUILD=1
-fi
+while [[ "$#" -gt 0 ]]
+do
+  case $1 in
+    --use-cache)
+      # Docker Buildkit
+      export DOCKER_BUILDKIT=1
+      export COMPOSE_DOCKER_CLI_BUILD=1
+      ;;
+  esac
+  shift
+done
 
 docker-compose -p $NETWORK up -d --build && \
 docker-compose -p $NETWORK logs -f
