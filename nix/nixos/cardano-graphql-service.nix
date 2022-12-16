f8fdcd18c4bf653f82c154e3c2832b9fbe83c431
Rhys Bartels-Waller <rhyslbw@gmail.com>
2022-12-17 02:31:46 +1100

    build: nixOS service changes
    Include cardano-graphql-background in build of cardano-graphql
    
     and include cardano-graphql-background-service.nix in module-list.nix
    
    fix cfg attr for cardano-graphql-background svc

--- nix/nixos/cardano-graphql-service.nix
+++ nix/nixos/cardano-graphql-service.nix
@@ -8,31 +8,6 @@ in {
     services.cardano-graphql = {
       enable = lib.mkEnableOption "cardano-explorer graphql service";
 
-      dbHost = lib.mkOption {
-        type = lib.types.str;
-        default = "/run/postgresql";
-      };
-
-      dbPassword = lib.mkOption {
-        type = lib.types.str;
-        default = ''""'';
-      };
-
-      dbPort = lib.mkOption {
-        type = lib.types.int;
-        default = 5432;
-      };
-
-      dbUser = lib.mkOption {
-        type = lib.types.str;
-        default = "cexplorer";
-      };
-
-      db = lib.mkOption {
-        type = lib.types.str;
-        default = "cexplorer";
-      };
-
       enginePort = lib.mkOption {
         type = lib.types.int;
         default = 9999;
@@ -105,15 +80,7 @@ in {
         default = null;
         description = "Source directory or file to generate allow-list from";
       };
-      metadataServerUri = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
-        default = null;
-      };
       pollingIntervalAdaSupply = lib.mkOption {
-        type = lib.types.nullOr lib.types.int;
-        default = null;
-      };
-      assetMetadataUpdateInterval = lib.mkOption {
         type = lib.types.nullOr lib.types.int;
         default = null;
       };
@@ -128,18 +95,10 @@ in {
     boolToNodeJSEnv = bool: if bool then "true" else "false";
     frontend = selfPkgs.packages.cardano-graphql;
     persistgraphql = selfPkgs.packages.persistgraphql;
-    hasura-cli = selfPkgs.packages.hasura-cli;
-    hasura-cli-ext = selfPkgs.packages.hasura-cli-ext;
     hasuraBaseUri = "${cfg.hasuraProtocol}://${cfg.hasuraIp}:${toString cfg.enginePort}";
     pluginLibPath = pkgs.lib.makeLibraryPath [
       pkgs.stdenv.cc.cc.lib
     ];
-    installHasuraCLI = ''
-      # always start with no plugins so future upgrades will work
-      rm -rf ~/.hasura/plugins
-      mkdir -p ~/.hasura/plugins/store/cli-ext/v${hasura-cli-ext.version}
-      ln -s ${hasura-cli-ext}/bin/cli-ext-hasura-linux ~/.hasura/plugins/store/cli-ext/v${hasura-cli-ext.version}/cli-ext-hasura-linux
-    '';
   in lib.mkIf cfg.enable {
     systemd.services.cardano-graphql = {
       wantedBy = [ "multi-user.target" ];
@@ -147,17 +106,11 @@ in {
       after = [ "graphql-engine.service" ];
       environment = lib.filterAttrs (k: v: v != null) {
         CARDANO_NODE_CONFIG_PATH = cfg.cardanoNodeConfigPath;
-        HASURA_CLI_PATH = hasura-cli + "/bin/hasura";
         HASURA_GRAPHQL_ENABLE_TELEMETRY = toString false;
         HASURA_URI = hasuraBaseUri;
         LOGGER_MIN_SEVERITY = cfg.loggerMinSeverity;
         OGMIOS_HOST = cfg.ogmiosHost;
         OGMIOS_PORT = toString cfg.ogmiosPort;
-        POSTGRES_DB = cfg.db;
-        POSTGRES_HOST = cfg.dbHost;
-        POSTGRES_PASSWORD = cfg.dbPassword;
-        POSTGRES_PORT = toString cfg.dbPort;
-        POSTGRES_USER = cfg.dbUser;
         PROMETHEUS_METRICS = boolToNodeJSEnv cfg.enablePrometheus;
         TRACING = boolToNodeJSEnv (cfg.enableTracing || cfg.enablePrometheus);
         ALLOW_INTROSPECTION = boolToNodeJSEnv cfg.allowIntrospection;
@@ -166,17 +119,11 @@ in {
       } //
       (lib.optionalAttrs (cfg.allowedOrigins != null) { ALLOWED_ORIGINS = cfg.allowedOrigins; }) //
       (lib.optionalAttrs (cfg.listenAddress != null) { LISTEN_ADDRESS = cfg.listenAddress; }) //
-      (lib.optionalAttrs (cfg.metadataServerUri != null) { METADATA_SERVER_URI = toString cfg.metadataServerUri; }) //
       (lib.optionalAttrs (cfg.pollingIntervalAdaSupply != null) { POLLING_INTERVAL_ADA_SUPPLY = toString cfg.pollingIntervalAdaSupply; }) //
-      (lib.optionalAttrs (cfg.assetMetadataUpdateInterval != null) { ASSET_METADATA_UPDATE_INTERVAL = toString cfg.assetMetadataUpdateInterval; }) //
       (lib.optionalAttrs (cfg.queryDepthLimit != null) { QUERY_DEPTH_LIMIT = toString cfg.queryDepthLimit; }) //
       (lib.optionalAttrs (cfg.allowListPath != null) { ALLOW_LIST_PATH = cfg.allowListPath; }) //
       (lib.optionalAttrs (cfg.maxQueryComplexity != null) { MAX_QUERY_COMPLEXITY = toString cfg.maxQueryComplexity; });
-      path = with pkgs; [ netcat curl postgresql frontend hasura-cli glibc.bin patchelf ];
-      preStart = ''
-        set -exuo pipefail
-        ${installHasuraCLI}
-      '';
+      path = with pkgs; [ netcat curl frontend glibc.bin patchelf ];
       script = ''
         exec cardano-graphql
       '';
