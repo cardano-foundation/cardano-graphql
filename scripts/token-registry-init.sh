40908fb00756cccb8d68c8afb78bba5ee61e6d59
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-06-03 15:52:41 +0200

    feat: added configurability which repository to use for the token-registry

--- scripts/token-registry-init.sh
+++ scripts/token-registry-init.sh
@@ -5,4 +5,17 @@ export DB_PASSWORD=$(cat /run/secrets/postgres_password)
 export DB_NAME=$(cat /run/secrets/postgres_db)
 export DB_URL=jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${DB_NAME}?currentSchema=${DB_SCHEMA}
 
+case "$NETWORK" in
+    mainnet)
+      GITHUB_ORGANIZATION=cardano-foundation
+      GITHUB_PROJECT_NAME=cardano-token-registry
+      GITHUB_MAPPINGS_FOLDER=mappings
+      ;;
+    preprod|preview|testnet)
+      GITHUB_ORGANIZATION=input-output-hk
+      GITHUB_PROJECT_NAME=metadata-registry-testnet
+      GITHUB_MAPPINGS_FOLDER=registry
+      ;;
+esac
+echo Using Github Repository $GITHUB_ORGANIZATION/$GITHUB_PROJECT_NAME as token registry
 java -jar /app/app.jar
