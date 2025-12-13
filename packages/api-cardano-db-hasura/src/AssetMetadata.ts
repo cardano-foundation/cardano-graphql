cc98f99bf92c4307084e5cc809c30a8cf9110b98
VladislavKudrin <vladislavkudrin@gmail.com>
2025-12-13 09:56:34 +0100

    feat: cip68 support for FTs

--- packages/api-cardano-db-hasura/src/AssetMetadata.ts
+++ packages/api-cardano-db-hasura/src/AssetMetadata.ts
@@ -1,38 +1,32 @@
+export type QueryPriority = 'CIP_68' | 'CIP_26'
 
-export interface Signature {
-  signature: string
-  publicKey: string
+export interface AssetV2 {
+  subject: string
+  metadata: AssetMetadata
+  queryPriority: QueryPriority[]
 }
 
 export interface AssetMetadata {
   decimals: {
     value: number
-    anSignatures: Signature[]
   }
   description?: {
     value: string
-    anSignatures: Signature[]
   }
   logo?: {
     value: string
-    anSignatures: Signature[]
   }
   name?: {
     value: string
-    anSignatures: Signature[]
   }
-  owner?: Signature
   preImage?: {
     value: string
     hashFn: string
   }
-  subject: string
   ticker?: {
     value: string
-    anSignatures: Signature[]
   }
   url?: {
     value: string
-    anSignatures: Signature[]
   }
 }
--- packages/api-cardano-db-hasura/src/MetadataClient.ts
+++ packages/api-cardano-db-hasura/src/MetadataClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 import { errors, RunnableModuleState } from '@cardano-graphql/util'
 import { dummyLogger, Logger } from 'ts-log'
-import { AssetMetadata } from './AssetMetadata'
+import { AssetV2 } from './AssetMetadata'
 import { Asset } from './graphql_types'
 import pRetry from 'p-retry'
 
@@ -12,7 +12,7 @@ export class MetadataClient {
   private axiosClient: AxiosInstance
   public state: RunnableModuleState
 
-  constructor (
+  constructor(
     enabled = false,
     private metadataServerUri: string,
     private logger: Logger = dummyLogger
@@ -25,11 +25,14 @@ export class MetadataClient {
         baseURL: this.metadataServerUri
       })
     } else {
-      this.logger.info({ module: MODULE_NAME }, 'MetadataClient disabled — skipping registry sync.')
+      this.logger.info(
+        { module: MODULE_NAME },
+        'MetadataClient disabled — skipping registry sync.'
+      )
     }
   }
 
-  private async ensureLocalMetadataServerIsAvailable (): Promise<void> {
+  private async ensureLocalMetadataServerIsAvailable(): Promise<void> {
     if (!this.enabled) return
     await pRetry(
       async () => {
@@ -39,28 +42,32 @@ export class MetadataClient {
           if (error.code === 'ENOTFOUND') {
             this.logger.info('Waiting for TokenRegistry to be available')
             throw new errors.HostDoesNotExist('metadata server')
-          } else if (error.response?.status === 400) { // Needed until TokenRegistry is updated
+          } else if (error.response?.status === 400) {
+            // Needed until TokenRegistry is updated
             this.logger.info('Token Registry is up')
           } else if (error.response?.status !== 404) {
             this.logger.info('Metadata Server unreachable.')
             throw error
           }
         }
-      }, {
+      },
+      {
         factor: 1.5,
         retries: 10
       }
     )
   }
 
-  private async waitForLocalMetadataServerSynced (): Promise<void> {
+  private async waitForLocalMetadataServerSynced(): Promise<void> {
     if (!this.enabled) return
     await pRetry(
       async () => {
         try {
           const result = await this.axiosClient.get('/health')
           if (!result.data.synced) {
-            this.logger.info('Metadata registry is still syncing. This can take up to 90 min...')
+            this.logger.info(
+              'Metadata registry is still syncing. This can take up to 90 min...'
+            )
             throw new Error('')
           }
         } catch (error) {
@@ -70,7 +77,8 @@ export class MetadataClient {
             throw new Error('')
           }
         }
-      }, {
+      },
+      {
         factor: 1.5,
         retries: 1000,
         minTimeout: 60000 // first try after one minute
@@ -78,25 +86,21 @@ export class MetadataClient {
     )
   }
 
-  public async fetch (assetIds: Asset['assetId'][]): Promise<AssetMetadata[]> {
+  public async fetch(assetIds: Asset['assetId'][]): Promise<AssetV2[]> {
     if (!this.enabled) {
-      this.logger.debug({ module: MODULE_NAME }, 'Skipping metadata fetch — client disabled.')
+      this.logger.debug(
+        { module: MODULE_NAME },
+        'Skipping metadata fetch — client disabled.'
+      )
       return []
     }
     if (this.state !== 'initialized') {
       throw new errors.ModuleIsNotInitialized(MODULE_NAME, 'fetch')
     }
     try {
-      const response = await this.axiosClient.post('metadata/query', {
+      const response = await this.axiosClient.post('/api/v2/subjects/query', {
         subjects: assetIds,
-        properties: [
-          'decimals',
-          'description',
-          'logo',
-          'name',
-          'ticker',
-          'url'
-        ]
+        properties: ['decimals', 'description', 'logo', 'name', 'ticker', 'url']
       })
       return response.data.subjects
     } catch (error) {
@@ -108,9 +112,12 @@ export class MetadataClient {
     }
   }
 
-  public async initialize () {
+  public async initialize() {
     if (!this.enabled) {
-      this.logger.info({ module: MODULE_NAME }, 'Skipping initialization — MetadataClient disabled.')
+      this.logger.info(
+        { module: MODULE_NAME },
+        'Skipping initialization — MetadataClient disabled.'
+      )
       this.state = 'initialized'
       return
     }
@@ -118,7 +125,10 @@ export class MetadataClient {
     this.state = 'initializing'
     this.logger.info({ module: MODULE_NAME }, 'Initializing')
     await this.ensureLocalMetadataServerIsAvailable()
-    this.logger.info({ module: MODULE_NAME }, 'Metadata Server is up and running. Checking Sync Status.')
+    this.logger.info(
+      { module: MODULE_NAME },
+      'Metadata Server is up and running. Checking Sync Status.'
+    )
     await this.waitForLocalMetadataServerSynced()
     this.state = 'initialized'
     this.logger.info({ module: MODULE_NAME }, 'Initialized')
